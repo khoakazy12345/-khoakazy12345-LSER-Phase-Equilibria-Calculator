2f71d6f6b7660289ba548087dddec6eadaab2911
Thomas Nguyen <nguyenk7@miamioh.com>
2021-09-26 01:07:06 -0400

    Update - Header + Logo + Basic Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import myImage from './logo.png';
+import { Image, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        Learn React
+    <div>
+      <header class="text-center mt-3">
+        <Image src={myImage} width={80} />
+        <h2>LSER Phase Equilibria Calculator</h2>
       </header>
+
+      <Form>
+        <Form.Select aria-label="Default select example">
+          <option>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </Form.Select>
+      </Form>
     </div>
   );
 }
