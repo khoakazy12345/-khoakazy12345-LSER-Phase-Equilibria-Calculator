1e54a7efcf8d8042505629f3e088a74bab469394
Thomas Nguyen <nguyenk7@miamioh.com>
2021-09-27 00:41:43 -0400

    Update - Caculation + Toast + Basic Function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
 import myImage from './logo.png';
-import { Image, Form, Container, Button, Row, Col } from 'react-bootstrap';
+import { Image, Form, Container, Button, Row, Col, Card } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
-import CASNumberList from './utilities';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const onSubmit = (data) => {
-  console.log(data);
-}
+import Utility from './Utility';
+
+const myUtility = new Utility();
+
 
 function App() {
-  const CASList = CASNumberList();
+  const CASList = myUtility.getCompoundNameList();
+  const [showCard, setShowCard] = useState(false);
+  const [logK, setLogK] = useState(false);
+
+  const onSubmit = (data) => {
+    const result = myUtility.calculateLogK(data["solvent"], data["solute"]);
+    setLogK(result);
+    setShowCard(true);
+  }
+
+  const onCloseCard = () => {
+    setShowCard(false);
+  }
 
   const { handleSubmit, register } = useForm({
     defaultValues: {
-      solvent: "0",
-      solute: "0",
+      solvent: 0,
+      solute: 0,
     },
   });
 
@@ -26,17 +39,22 @@ function App() {
         <h2>LSER Phase Equilibria Calculator</h2>
       </header>
 
+      {showCard &&
+        <div id="resultNoti">
+          <Card id="notificationCard">
+            <Card.Body>
+              <Card.Title>Result of the calculation</Card.Title>
+              <Card.Text>
+                Your logK is {logK}!
+              </Card.Text>
+              <Button variant="primary" onClick={onCloseCard}>Close</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      }
+
       <div className="mx-5 mt-3">
         <Form onSubmit={handleSubmit(onSubmit)}>
-          <Container className="mt-3">
-            <Form.Label>Select your solvent</Form.Label>
-            <Form.Select {...register("solvent")}>
-              {CASList.map((cas) => {
-                return <option value={cas}>{cas}</option>
-              })}
-            </Form.Select>
-          </Container>
-
           <Container className="mt-3">
             <Form.Label>Select your solute</Form.Label>
             <Form.Select {...register("solute")}>
@@ -46,7 +64,17 @@ function App() {
             </Form.Select>
           </Container>
 
+
           <Container className="mt-3">
+            <Form.Label>Select your solvent</Form.Label>
+            <Form.Select {...register("solvent")}>
+              {CASList.map((cas) => {
+                return <option value={cas}>{cas}</option>
+              })}
+            </Form.Select>
+          </Container>
+
+          <Container className="mt-10">
             <Col>
               <Row><Button id="button" variant="primary" size="lg" type="submit">Submit</Button></Row>
               <Row><Button id="button" variant="danger" size="lg">Reset</Button></Row>
---
+++ src/Utility.js
@@ -0,0 +1,45 @@
+class Utility   {
+    constructor() {
+        this.compoundData = require('./data.json');
+    }
+
+    getCompoundNameList() {
+        var compoundArray = [];
+        for (const compound of this.compoundData)    {
+            compoundArray.push(compound["Compound Name"]);
+        }
+    
+        return compoundArray;
+    }
+
+    /*
+    Solvent -> Compound 1
+    Solute -> Compound 2
+    Formula = c1 + e1*E2 + s1*S2 + a1*A2 + b1*B2 + l1*L2
+    */
+
+    calculateLogK(solvent, solute) {
+        let c1,e1,s1,a1,b1,l1,E2,S2,A2,B2,L2 = 0;
+        for (const compound of this.compoundData)    {
+            if (compound["Compound Name"] === solvent)  {
+                c1 = typeof compound["c"] === 'string' ? 0 : compound["c"] ;
+                e1 = typeof compound["e"] === 'string' ? 0 : compound["e"] ;
+                s1 = typeof compound["s"] === 'string' ? 0 : compound["s"] ;
+                a1 = typeof compound["a"] === 'string' ? 0 : compound["a"] ;
+                b1 = typeof compound["b"] === 'string' ? 0 : compound["b"] ;
+                l1 = typeof compound["l"] === 'string' ? 0 : compound["l"] ;
+            }
+            
+            if (compound["Compound Name"] === solute)  {
+                E2 = typeof compound["E"] === 'string' ? 0 : compound["E"] ;
+                S2 = typeof compound["S"] === 'string' ? 0 : compound["S"] ;
+                A2 = typeof compound["A"] === 'string' ? 0 : compound["A"] ;
+                B2 = typeof compound["B"] === 'string' ? 0 : compound["B"] ;
+                L2 = typeof compound["L"] === 'string' ? 0 : compound["L"] ;
+            }
+        }
+        return (c1 + e1*E2 + s1*S2 + a1*A2 + b1*B2 + l1*L2).toFixed(3);
+    }
+}
+
+export default Utility;
